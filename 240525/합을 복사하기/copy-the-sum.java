89f69fa883ff5028972da5956db6cadf00f1a287
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-05-25 13:29:09 +0900

    Title: 합을 복사하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240525/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a = b = c = 1+2+3;
+        System.out.printf("%d %d %d",a,b,c);
+
+    }
+}
