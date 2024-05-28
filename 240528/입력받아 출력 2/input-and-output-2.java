400f51a8bf6dd5c2889f6089d11e73c2909cabe3
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-05-28 15:55:58 +0900

    Title: 입력받아 출력 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1+""+num2);
+
+    }
+}
