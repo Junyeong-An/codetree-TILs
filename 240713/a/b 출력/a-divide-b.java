3f318e27d3a2e42eb270fbbd5c38929b1bd48b4e
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-07-13 19:26:52 +0900

    Title: a/b 출력, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240713/a/b 출력/a-divide-b.java
+++ 240713/a/b 출력/a-divide-b.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        double c = (double)a/b;
-        System.out.printf("%.20f",c);
+        
+        BigDecimal result = new BigDecimal((double) a / b);
+        result = result.setScale(21, RoundingMode.FLOOR);
+        
+        System.out.println(result.toPlainString());
+        
+        sc.close();
     }
 }
