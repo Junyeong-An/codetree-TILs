baaead53dc79e8a5c7ed754a2ac332001294dd56
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-07-29 14:36:46 +0900

    Title: 홀수만 더하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240729/홀수만 더하기/add-only-odd-numbers.java
+++ 240729/홀수만 더하기/add-only-odd-numbers.java
@@ -5,7 +5,7 @@ public class Main {
         int n = sc.nextInt();
         int sum = 0;
         for(int i=0;i<n;i++){
-            if(n%2==1 && n%3==0){
+            if(i%2==1 && i%3==0){
                 sum+=i;
             }
         }
