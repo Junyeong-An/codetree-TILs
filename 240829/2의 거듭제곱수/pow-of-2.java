84689ca7169e3b382d02d8f545b5ba9cd6a9e084
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-08-29 23:30:13 +0900

    Title: 2의 거듭제곱수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240829/2의 거듭제곱수/pow-of-2.java
+++ 240829/2의 거듭제곱수/pow-of-2.java
@@ -3,7 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int x = n/2;
+        int x = 0;
+
+        while(n>1){
+            n=n/2;
+            x++;
+        }
         System.out.print(x);
+        
+        sc.close();
     }
 }
