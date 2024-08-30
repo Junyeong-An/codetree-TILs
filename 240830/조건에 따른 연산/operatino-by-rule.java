eb8eafef0537c00a6460812c02670b731855734b
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-08-30 21:46:48 +0900

    Title: 조건에 따른 연산, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240830/조건에 따른 연산/operatino-by-rule.java
+++ 240830/조건에 따른 연산/operatino-by-rule.java
@@ -5,7 +5,7 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
 
-        while(n<=1000){
+        while(n<1000){
             if(n%2==0){
                 n=n*3+1;
                 cnt++;
